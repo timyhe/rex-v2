602552e6efc7a5689e41a5ebe7b4db9c91ca1065
Tim He <the@seatgeek.com>
2025-02-25 13:41:20 -0500

    trying to fix lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={roobert.variable}>
+    <html lang="en" className={`${roobert.variable} ${geistSans.variable} ${geistMono.variable}`}>
       <body>{children}</body>
     </html>
   )
